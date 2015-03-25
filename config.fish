8f4c7de7b3f469316093069b0a5f5cec00a73f96
Bob Clewell <bclewell@weblinc.com>
2015-03-24 22:40:57 -0400

    Added alias for FAM Brands.

--- config.fish
+++ config.fish
@@ -7,10 +7,13 @@ alias uspolo="cd ~/src/uspolo"
 alias hobo="cd ~/src/hobo"
 alias mj="cd ~/src/mintjulep"
 alias her="cd ~/src/herrington"
+alias sg="cd ~/src/sgangster"
+alias fam="cd ~/src/fambrands"
 
 # WebLinc Cores and Internal Sites
 alias wl="cd ~/src/weblinc"
 alias wl03="cd ~/src/weblinc_0-3-7"
+alias wl04="cd ~/src/weblinc_0-4"
 alias wl05="cd ~/src/weblinc_0-5"
 alias wld="cd ~/src/dummy-app"
 
@@ -19,4 +22,4 @@ alias snips="cd /Users/bclewell/Library/Application\ Support/Sublime\ Text\ 2/Pa
 alias fish="cd ~/.config/fish/"
 
 # Searching Rails Logs
-alias applog="powder applog | grep >>>>>"
+# alias applog="powder applog | grep >>>>>"
