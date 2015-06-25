ae7b5d10f03ecb15965149af632cbbf28c88a15f
Bob Clewell <bclewell@weblinc.com>
2015-06-25 10:09:56 -0400

    Changed to Oh My Fish. New config stuff.

--- config.fish
+++ config.fish
@@ -1,29 +1,29 @@
+# Path to your oh-my-fish.
+set fish_path $HOME/.oh-my-fish
+
+# Theme
+set fish_theme robbyrussell
+
+# All built-in plugins can be found at ~/.oh-my-fish/plugins/
+# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
+# Enable plugins by adding their name separated by a space to the line below.
+set fish_plugins theme
+
+# Path to your custom folder (default path is ~/.oh-my-fish/custom)
+#set fish_custom $HOME/dotfiles/oh-my-fish
+
+# Load oh-my-fish configuration.
+. $fish_path/oh-my-fish.fish
+
+# Alias for the fuck
+function fuck
+    eval (thefuck $history[1])
+end
+
+# For autojump
+[ -f /usr/local/share/autojump/autojump.fish ]; and . /usr/local/share/autojump/autojump.fish
+
 # Command Shortcuts
 alias be="bundle exec"
 alias del="rmtrash"
-
-# WebLinc Customer Sites
-alias uspolo="cd ~/src/uspolo"
-alias hobo="cd ~/src/hobo"
-alias mj="cd ~/src/mintjulep"
-alias her="cd ~/src/herrington"
-alias sg="cd ~/src/sgangster"
-alias fam="cd ~/src/fambrands"
-alias dib="cd ~/src/doitbest"
-
-# Bob's Sites
-alias pick="cd ~/bob/pickem"
-
-# WebLinc Cores and Internal Sites
-alias wl="cd ~/src/weblinc"
-alias wl03="cd ~/src/weblinc_0-3-7"
-alias wl04="cd ~/src/weblinc_0-4"
-alias wl05="cd ~/src/weblinc_0-5-13"
-alias wld="cd ~/src/dummy-app"
-
-# Config Shortcuts
-alias snips="cd /Users/bclewell/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/"
-alias fish="cd ~/.config/fish/"
-
-# Searching Rails Logs
-# alias applog="powder applog | grep >>>>>"
+alias g="git"
